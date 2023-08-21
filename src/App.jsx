432753cd879e1af8dd0e50968683ed7281ed8931
Rasik Abdullah <rasikabdullah@gmail.com>
2023-08-21 14:48:24 +0600

    navbar aded

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
-
 import './App.css'
+import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Routes from './components/Routes'
 import Footer from './components/Footer'
@@ -9,9 +8,9 @@ function App() {
   const [darkTheme, setDarkTheme] = useState(false)
 
   return (
-    <div className={darkTheme ? 'dark' : ''}>
-      <div className='bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen'>
-        <Navbar />
+    <div className={darkTheme? 'dark' : ''}>
+      <div className=" bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+        <Navbar  setDarkTheme={setDarkTheme} darkTheme={darkTheme}  />
         <Routes />
         <Footer />
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,19 @@
+import { Link } from "react-router-dom"
+import { AiOutlineSearch } from "react-icons/ai";
 
-
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+const Navbar = ({ setDarkTheme, darkTheme }) => {
+    return (
+        <div className="p-5 pb-2 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
+            <div className="flex justify-between items-center space-x-5 w-screen ">
+                <Link to='/'>
+                    <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-50 dark:text-gray-900 flex items-center gap-2">
+                        Fake Google <AiOutlineSearch/>
+                    </p>
+                </Link>
+                <button type="button" onClick={() => setDarkTheme(!darkTheme)} className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg">{darkTheme ? '💡 Light' : '🌙 Dark'}</button>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
